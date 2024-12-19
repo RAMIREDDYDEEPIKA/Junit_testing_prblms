9c9296d1a2d8ee558efdd8070be553a2fc041450
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-12-19 17:15:01 +0530

    Reading csv files

---
+++ ReadCsvFile.java
@@ -0,0 +1,28 @@
+package sampleMavenProject.fileHandling;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ReadCsvFile
+{
+    @Test
+    public void testReadFile() throws IOException
+    {
+        File file=new File("Welcome.csv");
+        try(BufferedReader bReader=new BufferedReader(new FileReader(file)))
+        {
+            String data;
+            while((data=bReader.readLine())!=null){
+                System.out.println(data);
+            }
+        }
+        assertTrue(file.canRead());
+        assertTrue(file.exists());
+    }
+}

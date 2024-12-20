228f438acecf0f01214d7f0f2aeb94377fed91b6
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-12-20 14:49:45 +0530

    create jsonFile and done the serialization and deserialization

--- GsonTest.java
+++ GsonTest.java
@@ -5,49 +5,71 @@ import com.google.gson.GsonBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class Person{
+class Person {
     String name;
     String address;
     String phnNum;
-    public Person(String name,String address,String phnNum){
-        this.name=name;
-        this.address=address;
-        this.phnNum=phnNum;
+
+    public Person(String name, String address, String phnNum) {
+        this.name = name;
+        this.address = address;
+        this.phnNum = phnNum;
     }
 }
+
 public class GsonTest {
 
-    @Test
-    public void testingGsonFile() throws IOException {
+    private final Path jsonFilePath = Paths.get("person.json");
 
-        Path gsonFile=Files.createTempFile("Persons",".json");
-        System.out.println("path of file: "+gsonFile.toAbsolutePath());
-
-        Person person=new Person("Pooji","Bangalore","247964082469");
-
-        Gson personFile=new GsonBuilder().setPrettyPrinting().create();
-        String jsonFile= personFile.toJson(person);
-
-        try(BufferedWriter bWriter=new BufferedWriter(new FileWriter(gsonFile.toFile()))){
-            bWriter.write(jsonFile);
+    private void createJsonFile() throws IOException {
+        if (!jsonFilePath.toFile().exists()) {
+            jsonFilePath.toFile().createNewFile();
         }
+    }
 
-        System.out.println("File reading:");
-        try(BufferedReader bReader=new BufferedReader(new FileReader(gsonFile.toFile()))){
-            String data;
-            while((data=bReader.readLine())!=null){
-                System.out.println(data);
+    private void serializeToJson(Person person) throws IOException {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String jsonData = gson.toJson(person);
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(jsonFilePath.toFile()))) {
+            writer.write(jsonData);
+        }
+    }
+
+    private Person deserializeFromJson() throws IOException {
+        Gson gson = new Gson();
+        StringBuilder jsonData = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(jsonFilePath.toFile()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                jsonData.append(line);
             }
         }
 
-        assertTrue(gsonFile.toFile().exists());
+        return gson.fromJson(jsonData.toString(), Person.class);
+    }
 
-        Person deserialize=personFile.fromJson(jsonFile.toString(), Person.class);
-        System.out.println("\nDeserialization : "+deserialize.name+" , "+deserialize.address+" , "+deserialize.phnNum);
+    @Test
+    public void testSerializationAndDeserialization() throws IOException {
+        createJsonFile();
+
+        Person person = new Person("Pooji", "Bangalore", "247964082469");
+
+        serializeToJson(person);
+
+        Person deserializedPerson = deserializeFromJson();
+
+        assertEquals(person.name, deserializedPerson.name);
+        assertEquals(person.address, deserializedPerson.address);
+        assertEquals(person.phnNum, deserializedPerson.phnNum);
+
+        System.out.println("Serialization and Deserialization successful.");
+        System.out.println("Deserialized Data: " + deserializedPerson.name + " , " + deserializedPerson.address + " , " + deserializedPerson.phnNum);
     }
 }

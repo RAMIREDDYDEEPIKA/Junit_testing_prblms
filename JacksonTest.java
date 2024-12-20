4be999661d71fd442621b9c4a0ae113fb2cb8b79
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-12-20 14:51:08 +0530

    create jsonFile and done serialization and deserialization using Jackson

--- JacksonTest.java
+++ JacksonTest.java
@@ -1,54 +1,77 @@
 package sampleMavenProject.fileHandling;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class Student{
-
+class Student {
     public String name;
     public String section;
     public String course;
 
-    public Student(){
-
-    }
-    public Student(String name,String section,String course){
-        this.name=name;
-        this.section=section;
-        this.course=course;
+    public Student() {
     }
 
+    public Student(String name, String section, String course) {
+        this.name = name;
+        this.section = section;
+        this.course = course;
+    }
     @Override
     public String toString() {
         return "Student{" +
-                "name=" + name +
-                ", section=" + section +
-                ", course=" + course +
+                "name='" + name + '\'' +
+                ", section='" + section + '\'' +
+                ", course='" + course + '\'' +
                 '}';
     }
 }
 
-public class JacksonTest
-{
+public class JacksonTest {
+
+    private final File jsonFile = new File("student.json");
+
+    private void createJsonFile(Student student) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.writeValue(jsonFile, student);
+    }
+
+    public void serializeToJson(Student student) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonData = objectMapper.writeValueAsString(student);
+        objectMapper.writeValue(jsonFile, student);
+        System.out.println("Serialized Data: " + jsonData);
+    }
+
+    public Student deserializeFromJson() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(jsonFile, Student.class);
+    }
+
     @Test
-    public void testJackson() throws JsonProcessingException
-    {
-        ObjectMapper jacksonFile=new ObjectMapper();
-        Student data=new Student("Bhavya","B","Java full stack");
+    public void testSerialization() throws IOException {
+        Student student = new Student("Bhavya", "B", "Java full stack");
 
-        String jsonFile= jacksonFile.writeValueAsString(data);
-        System.out.println("Serialization: "+jsonFile);
+        serializeToJson(student);
 
-        Student objectFile=jacksonFile.readValue(jsonFile,Student.class);
-        System.out.println("Deserialization: "+objectFile);
+        assertNotNull(jsonFile);
+        assertEquals(true, jsonFile.exists());
+    }
 
-        assertNotNull(objectFile);
-        assertEquals(data.name,objectFile.name);
-        assertEquals(data.section,objectFile.section);
-        assertEquals(data.course,objectFile.course);
+    @Test
+    public void testDeserialization() throws IOException {
+        Student deserializedStudent = deserializeFromJson();
+
+        assertNotNull(deserializedStudent);
+        assertEquals("Bhavya", deserializedStudent.name);
+        assertEquals("B", deserializedStudent.section);
+        assertEquals("Java full stack", deserializedStudent.course);
+
+        System.out.println("Deserialization : " + deserializedStudent);
     }
 }
